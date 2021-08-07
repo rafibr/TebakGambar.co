5fbcb6780bda88562684fa00c2a2570565d662a0
Siti Rohmah <strohmahafiz@gmail.com>
2021-08-07 18:10:47 +0800

    profile page

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -1,33 +1,127 @@
 <template>
-    <div>
-        <section v-if="id">
+  <!-- <section v-if="id">
             <h1>Hello {{ detailuser.name }}</h1>
             <h3>Email: {{ detailuser.email }}</h3>
-            <!-- <router-link :to="{ username: 'User' }">Back</router-link> -->
             <router-link to="/user">Back</router-link>
-        </section>
-        <div class="info-box"><span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span> <div class="info-box-content"><span class="info-box-text">Messages</span> <span class="info-box-number">1,410</span></div></div>
+        </section> -->
+
+  <div class="no">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
     </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+
+    <section class="content">
+      <div class="container">
+        <h5 class="mb-2">Info Box</h5>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-info"
+                ><i class="far fa-envelope"></i
+              ></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Messages</span>
+                <span class="info-box-number">1,410</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-success"
+                ><i class="far fa-flag"></i
+              ></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Bookmarks</span>
+                <span class="info-box-number">410</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-warning"
+                ><i class="far fa-copy"></i
+              ></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Uploads</span>
+                <span class="info-box-number">13,648</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-danger"
+                ><i class="far fa-star"></i
+              ></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Likes</span>
+                <span class="info-box-number">93,139</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+
+    <!-- /.content -->
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["id"],
-    data() {
-        return {
-            detailuser: {}
-        };
+  props: ["id"],
+  data() {
+    return {
+      detailuser: {},
+    };
+  },
+  mounted() {
+    this.getUser();
+  },
+  methods: {
+    getUser() {
+      axios.get("/api/users/" + this.id).then((response) => {
+        console.log(response.data["data"]);
+        this.detailuser = response.data["data"];
+      });
     },
-    mounted() {
-        this.getUser();
-    },
-    methods: {
-        getUser() {
-            axios.get("/api/users/" + this.id).then(response => {
-                console.log(response.data["data"]);
-                this.detailuser = response.data["data"];
-            });
-        }
-    }
+  },
 };
 </script>
