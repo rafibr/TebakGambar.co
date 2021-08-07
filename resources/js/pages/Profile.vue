58b28a975d92cab74159a7cb8ed34d43f9e736ee
Muhammad Rafi B.R <rafi.brilliansyah@gmail.com>
2021-08-07 18:16:21 +0800

    profile page fixed

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -1,127 +1,201 @@
 <template>
-  <!-- <section v-if="id">
+    <!-- <section v-if="id">
             <h1>Hello {{ detailuser.name }}</h1>
             <h3>Email: {{ detailuser.email }}</h3>
             <router-link to="/user">Back</router-link>
         </section> -->
 
-  <div class="no">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard</h1>
-          </div>
-          <!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-          </div>
-          <!-- /.col -->
+    <div class="no">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container -->
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+
+        <section class="content">
+            <div class="container">
+                <h5 class="mb-2">Info Box</h5>
+                <div class="row">
+                    <div class="col-md-4 col-12">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">About Me</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <strong
+                                    ><i class="fas fa-book mr-1"></i>
+                                    Education</strong
+                                >
+
+                                <p class="text-muted">
+                                    B.S. in Computer Science from the University
+                                    of Tennessee at Knoxville
+                                </p>
+
+                                <hr />
+
+                                <strong
+                                    ><i class="fas fa-map-marker-alt mr-1"></i>
+                                    Location</strong
+                                >
+
+                                <p class="text-muted">Malibu, California</p>
+
+                                <hr />
+
+                                <strong
+                                    ><i class="fas fa-pencil-alt mr-1"></i>
+                                    Skills</strong
+                                >
+
+                                <p class="text-muted">
+                                    <span class="tag tag-danger"
+                                        >UI Design</span
+                                    >
+                                    <span class="tag tag-success">Coding</span>
+                                    <span class="tag tag-info">Javascript</span>
+                                    <span class="tag tag-warning">PHP</span>
+                                    <span class="tag tag-primary">Node.js</span>
+                                </p>
+
+                                <hr />
+
+                                <strong
+                                    ><i class="far fa-file-alt mr-1"></i>
+                                    Notes</strong
+                                >
+
+                                <p class="text-muted">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Etiam fermentum enim neque.
+                                </p>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-12">
+                        <table>
+                            <tr>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                                <td>
+                                    <h5>test</h5>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container -->
+        </section>
+
+        <!-- /.content -->
     </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-
-    <section class="content">
-      <div class="container">
-        <h5 class="mb-2">Info Box</h5>
-        <div class="row">
-          <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box">
-              <span class="info-box-icon bg-info"
-                ><i class="far fa-envelope"></i
-              ></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Messages</span>
-                <span class="info-box-number">1,410</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-          <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box">
-              <span class="info-box-icon bg-success"
-                ><i class="far fa-flag"></i
-              ></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Bookmarks</span>
-                <span class="info-box-number">410</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-          <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box">
-              <span class="info-box-icon bg-warning"
-                ><i class="far fa-copy"></i
-              ></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Uploads</span>
-                <span class="info-box-number">13,648</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-          <div class="col-md-3 col-sm-6 col-12">
-            <div class="info-box">
-              <span class="info-box-icon bg-danger"
-                ><i class="far fa-star"></i
-              ></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Likes</span>
-                <span class="info-box-number">93,139</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-          </div>
-          <!-- /.col -->
-        </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container -->
-    </section>
-
-    <!-- /.content -->
-  </div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
-  data() {
-    return {
-      detailuser: {},
-    };
-  },
-  mounted() {
-    this.getUser();
-  },
-  methods: {
-    getUser() {
-      axios.get("/api/users/" + this.id).then((response) => {
-        console.log(response.data["data"]);
-        this.detailuser = response.data["data"];
-      });
+    props: ["id"],
+    data() {
+        return {
+            detailuser: {}
+        };
     },
-  },
+    mounted() {
+        this.getUser();
+    },
+    methods: {
+        getUser() {
+            axios.get("/api/users/" + this.id).then(response => {
+                console.log(response.data["data"]);
+                this.detailuser = response.data["data"];
+            });
+        }
+    }
 };
 </script>
