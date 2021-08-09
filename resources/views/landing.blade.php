94af42db006f894fb267609bd1c10b2019fd78a3
Muhammad Rafi B.R <rafi.brilliansyah@gmail.com>
2021-08-09 20:14:17 +0800

    [delete] script.js

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -93,7 +93,6 @@
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
-    <script src="{{ asset('js/scripts.js') }}"></script>
 
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
