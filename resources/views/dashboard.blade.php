7f3d8899ae3e026cbc0b1c75fdf20a7cef55c5a0
Muhammad Rafi B.R <rafi.brilliansyah@gmail.com>
2021-08-12 06:57:28 +0800

    [add] widget idna

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
             <div class="container">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
+                        <h1 class="m-0"> <small>Dashboard</small></h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -30,71 +30,11 @@
         <div class="content">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                    <div class="col-md-5">
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">CPU Traffic</span>
-                                <span class="info-box-number">
-                                    10
-                                    <small>%</small>
-                                </span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <div class="info-box mb-3">
-                            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text">Likes</span>
-                                <span class="info-box-number">41,410</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-
-                    <!-- fix for small devices only -->
-                    <div class="clearfix hidden-md-up"></div>
-
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <div class="info-box mb-3">
-                            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text">Sales</span>
-                                <span class="info-box-number">760</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-12 col-sm-6 col-md-3">
-                        <div class="info-box mb-3">
-                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
-
-                            <div class="info-box-content">
-                                <span class="info-box-text">New Members</span>
-                                <span class="info-box-number">2,000</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
                         <div class="card card-secondary">
                             <div class="card-header">
-                                <h3 class="card-title">Primary</h3>
+                                <h3 class="card-title">Ticker List</h3>
 
                                 <div class="card-tools">
                                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -105,14 +45,69 @@
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body bg-dark">
-                                The body of the card
+                                <script src="https://widgets.coingecko.com/coingecko-coin-market-ticker-list-widget.js"></script>
+                                <coingecko-coin-market-ticker-list-widget coin-id="idena" currency="idr" locale="en">
+                                </coingecko-coin-market-ticker-list-widget>
                             </div>
                             <!-- /.card-body -->
                         </div>
+
+
+                    </div>
+                    <div class="col-md-7">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card card-secondary">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Idena Price</h3>
+
+                                        <div class="card-tools">
+                                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                <i class="fas fa-minus"></i>
+                                            </button>
+                                        </div>
+                                        <!-- /.card-tools -->
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body bg-dark">
+                                        <script src="https://widgets.coingecko.com/coingecko-coin-price-static-headline-widget.js"></script>
+                                        <coingecko-coin-price-static-headline-widget coin-ids="bitcoin,idena" currency="idr"
+                                            locale="en">
+                                        </coingecko-coin-price-static-headline-widget>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card card-secondary">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Idena Chart</h3>
+
+                                        <div class="card-tools">
+                                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                <i class="fas fa-minus"></i>
+                                            </button>
+                                        </div>
+                                        <!-- /.card-tools -->
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body bg-dark">
+                                        <script src="https://widgets.coingecko.com/coingecko-coin-compare-chart-widget.js"></script>
+                                        <coingecko-coin-compare-chart-widget coin-ids="bitcoin,idena,ethereum"
+                                            currency="idr" locale="en"></coingecko-coin-compare-chart-widget>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
                 <!-- /.row -->
+
             </div><!-- /.container-fluid -->
         </div>
         <!-- /.content -->
